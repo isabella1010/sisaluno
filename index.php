969a0b1e25016c219ff1ca2fc22b1b9e787b782a
ISABELLA <129870612+isabella1010@users.noreply.github.com>
2023-08-09 10:59:16 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>SisAluno - Página Inicial</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+        <h1>SisAluno - Página Inicial</h1>
+        <div class="menu">
+            <a href="aluno/index.php">Gerenciar Alunos</a>
+            <br>
+            <br>
+            <a href="professor/index.php">Gerenciar Professores</a>
+            <br>
+            <br>
+            <a href="disciplina/index.php">Gerenciar Disciplinas</a>
+        </div>
+</body>
+</html>
